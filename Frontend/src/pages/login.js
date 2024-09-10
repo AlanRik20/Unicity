a667ca0c8e8e6538eac40d5a03e8e2af242730c1
Leonardo <leoonardoogzz@gmail.com>
2024-09-10 12:05:35 -0600

    Se arreglo errores de estilo

--- Frontend/src/pages/login.js
+++ Frontend/src/pages/login.js
@@ -2,21 +2,21 @@ export function LoginPage() {
   const $container = document.createElement("div");
 
   $container.innerHTML = `
-<div class="flex justify-center items-center h-screen bg-violet-400">
+<div class="flex justify-center items-center h-screen ">
   <div class=" p-8 rounded-lg shadow-lg w-full max-w-md">
     <h2 class="text-2xl font-bold text-center mb-6">Registro</h2>
     <form id="loginForm" class="space-y-4">
       <div>
         <label class="block text-sm font-medium text-gray-700">Nombre</label>
-        <input type="text" id="name" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Ingresa tu nombre" required>
+        <input type="text" id="name" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm" placeholder="Ingresa tu nombre" required>
       </div>
      
-        <label class="block text-sm font-medium text-gray-700">Contraseña</label>
-        <input type="password" id="password" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Ingresa tu contraseña" required>
-        <button type="submit" class="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <label class="block text-sm font-medium text-black-700">Contraseña</label>
+        <input type="password" id="password" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm" placeholder="Ingresa tu contraseña" required>
+        <button type="submit" class="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
           Iniciar Sesión
         </button>
-        <p class="text-indigo-800">¿No tiene una cuenta? 
+        <p class="text-blue-800">¿No tiene una cuenta? 
         
         <a href="/register" class="text-lg hover:underline transition-colors duration-200 ${
           location.pathname === "/register" ? "text-pink-700" : ""
